c59d59ac00eebe20c0147df299047020d3c8d869
Marten Sigwart <marten@okamaja.de>
2019-12-18 18:05:43 +0100

    change type of arrays

--- init.sql
+++ init.sql
@@ -2,6 +2,6 @@ CREATE TABLE blockheader (
     block_hash varchar(64) primary key,
     block_number bigint,
     block_data jsonb,
-    dataset_lookup bigint[],
-    witness_lookup bigint[]
+    dataset_lookup text[],
+    witness_lookup text[]
 );
